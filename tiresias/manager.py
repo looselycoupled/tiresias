05852c91e25efeedfb2c0b90e1081917b904e8cf
looselycoupled <allen.leis@gmail.com>
2019-04-23 10:51:59 -0400

    adds single process flask manager

--- tiresias/manager.py
+++ tiresias/manager.py
@@ -2,6 +2,12 @@ import time
 import multiprocessing as mp
 from tiresias.utils.logger import LoggableMixin
 
+# For Demo
+import threading
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+
 SAMPLE_LOGGING_INTERVAL = 100
 SAMPLE_DURATION = 0.0096
 
@@ -67,6 +73,67 @@ class Manager(LoggableMixin):
             for p in procs:
                 p.join()
             self.logger.info("Manager: exiting")
+
+    def end(self):
+        pass
+
+
+
+##########################################################################
+# Flask
+##########################################################################
+
+app = Flask(__name__, template_folder="consumers/templates")
+app.config['SECRET_KEY'] = '4949f8f5beb339df992906c4218714e4#'
+socketio = SocketIO(app)
+thread = None
+
+@app.route('/')
+def render_index():
+    return render_template('index.html')
+
+class FlaskManager(LoggableMixin):
+
+    def __init__(self, sensors, consumers):
+        self.sensors = sensors
+        self.consumers = consumers
+        super(FlaskManager, self).__init__()
+
+    def _setup(self):
+        for s in self.sensors:
+            s.setup()
+        for c in self.consumers:
+            c.setup()
+
+
+
+    def start(self):
+        global thread
+        global socketio
+        global app
+
+        self._setup()
+
+        thread = threading.Thread(target=socketio.run, args=(app, ), kwargs={"host": "0.0.0.0", "debug": False})
+        thread.daemon = True
+        thread.start()
+
+        try:
+
+            while True:
+                start = time.time()
+                data = {"time": { "start": int(start * 1e6), "scale": "microsecond"}}
+
+                for s in self.sensors:
+                    data.update(s.read())
+                    if hasattr(s, "status"):
+                        data.update(s.status())
+
+                socketio.emit('update', data)
+                time.sleep(.5)
+
+        except KeyboardInterrupt:
+            self.logger.info("Manager: exit requested")
 
     def end(self):
         pass
